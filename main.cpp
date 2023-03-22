c686a03f9f37da843b773027ce337c54d2b9453c
NourMuhammad10 <101945184+NourMuhammad10@users.noreply.github.com>
2023-03-22 14:28:15 +0200

    Added comments

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int ch_MAX = 26;
+
 
 namespace sortlib {
     template<typename T>
+    ///@ Author: Nour Muhammad
+    /// time complexity: best case: O(n^2), worst case: O(n^2), average case: O(n^2)
     void selectionSort(T arr[], int size) {
         for (int i = 0; i < size - 1; ++i) {
             int minIndex = i;
@@ -37,6 +39,8 @@ namespace sortlib {
     }
 
     template<typename T>
+    ///@ Author: Nour Muhammad
+    /// time complexity: best case: O(n*logn), worst case: O(n^2), average case:  O(n*logn)
     void quickSort(T arr[], int lowerBound, int upperBound){
         if(lowerBound < upperBound){
             int location = partition(arr, lowerBound, upperBound);
@@ -45,46 +49,6 @@ namespace sortlib {
         }
     }
 }
-
-    string randomString(int ch) {
-        char alpha[ch_MAX] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
-                              'h', 'i', 'j', 'k', 'l', 'm', 'n',
-                              'o', 'p', 'q', 'r', 's', 't', 'u',
-                              'v', 'w', 'x', 'y', 'z'};
-
-        string result = "";
-        for (int i = 0; i < ch; i++) {
-            result = result + alpha[rand() % ch_MAX];
-        }
-        return result;
-    }
-
-    void generateStringArray(string arr[], int size) {
-        for (int i = 0; i < size; ++i) {
-            arr[i] = randomString(5);
-        }
-    }
-
-    template<typename T>
-    void generate_array(T arr[], int size, int low, int high) {
-        if (typeid(T) == typeid(char)) {
-            for (int i = 0; i < size; ++i) {
-                arr[i] = rand() % 26 + 'a';
-            }
-        } else {
-            for (int i = 0; i < size; i++) {
-                T r = low + static_cast<T>(rand()) * static_cast<T>(high - low) / RAND_MAX;
-                arr[i] = r;
-            }
-        }
-    }
-
-    template<typename T>
-    void displayArray(T arr, int size) {
-        for (int i = 0; i < size; ++i) {
-            cout << arr[i] << " ";
-        }
-    }
 
     using namespace std::chrono;
     using namespace sortlib;
@@ -97,6 +61,9 @@ namespace sortlib {
             selectionSort(arr, n);
             auto stop = high_resolution_clock::now();
             auto duration = duration_cast<microseconds>(stop - start);
+            for(int i=0;i<n;i++){
+                cout<<arr[i]<<' ';
+            }
             cout << endl;
             cout << "Time taken by selection sort: " << double(double(duration.count()) / 1000) << " milliseconds"<< endl;
         }
@@ -107,6 +74,9 @@ namespace sortlib {
             quickSort(arr, 0, n-1);
             auto stop = high_resolution_clock::now();
             auto duration = duration_cast<microseconds>(stop - start);
+            for(int i=0;i<n;i++){
+                cout<<arr[i]<<' ';
+            }
             cout << endl;
             cout << "Time taken by quicksort: " << double(double(duration.count()) / 1000) << " milliseconds"<< endl;
         }
@@ -114,8 +84,6 @@ namespace sortlib {
     };
 
 
-    using namespace sortlib;
+int main() {
 
-    int main() {
-
-    }
+}
